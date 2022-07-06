98a7ae8ad6935e4d4cfee5ef0877f1f12e5e9cdb
Joserbp <48876427+Joserbp@users.noreply.github.com>
2022-07-06 18:20:10 -0500

    Add project files.

---
+++ BL/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BL
+{
+    public class Alumno
+    {
+        public static string Add(ML.Alumno alumno)
+        {
+            string result = "";
+            using(SqlConnection context = new SqlConnection(DL.Conexion.Get())){
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "INSERT INTO Alumno(Nombre, Genero) VALUES(@Nombre, @Genero)";
+                cmd.Connection = context;
+
+                SqlParameter[] collection = new SqlParameter[2];
+                collection[0] = new SqlParameter( "Nombre", SqlDbType.VarChar );
+                collection[0].Value = alumno.Nombre;
+                collection[1] = new SqlParameter("Genero", SqlDbType.Char);
+                collection[1].Value = alumno.Genero;
+
+                cmd.Parameters.AddRange(collection);
+
+                cmd.Connection.Open();
+
+                int RowAffected = cmd.ExecuteNonQuery();
+
+                if (RowAffected > 0)
+                {
+                    result = "Alumno agregado exitosamente";
+                }
+                else
+                {
+                    result = "Error al realizar el insert";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Alumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Ingrese el nombre del alumno");
+            alumno.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingreso el genero del alumno");
+            alumno.Genero = Console.ReadLine();
+
+            string result = BL.Alumno.Add(alumno);
+
+            Console.WriteLine(result);
+        }
+    }
+}
