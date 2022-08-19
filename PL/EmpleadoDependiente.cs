f48934ac98bf65d51a9d0ebaaa21faa0311b806d
Joserbp <josera.jack@gmail.com>
2022-08-19 13:49:25 -0500

    EmpleadoDependiente

---
+++ PL/EmpleadoDependiente.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL_MVC.Controllers
+{
+    public class EmpleadoDependienteController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Empleado empleado = new ML.Empleado();
+            ML.Result resultEmpresas = BL.Empresa.GetAllEF();
+            ML.Result resultEmpleados = BL.Empleado.GetAll();
+            if (resultEmpresas.Correct)
+            {
+                empleado.Empresa = new ML.Empresa();
+                if (resultEmpleados.Correct)
+                {
+                    empleado.Empleados = resultEmpleados.Objects;
+                    empleado.Empresa.Empresas = resultEmpresas.Objects;
+                    return View(empleado);
+                }
+                else
+                {
+                    //error
+                }
+            }
+            else
+            {
+                //error
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetAll(ML.Empresa empresa)
+        {
+            ML.Empleado empleado = new ML.Empleado();
+            ML.Result resultEmpresas = BL.Empresa.GetAllEF();
+            ML.Result resultEmpleados = new ML.Result();
+            if (empresa.IdEmpresa == 0)
+            {
+                resultEmpleados = BL.Empleado.GetAll();
+            }
+            else
+            {
+                resultEmpleados = BL.Empleado.GetByIdEmpresa(empresa.IdEmpresa);
+            }
+            if (resultEmpresas.Correct)
+            {
+                empleado.Empresa = new ML.Empresa();
+                if (resultEmpleados.Correct)
+                {
+                    empleado.Empleados = resultEmpleados.Objects;
+                    empleado.Empresa.Empresas = resultEmpresas.Objects;
+                    return View(empleado);
+                }
+                else
+                {
+                    //error
+                }
+            }
+            else
+            {
+                //error
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dependiente(string numeroEmpleado)
+        {
+            if (numeroEmpleado == null)
+            {
+                numeroEmpleado = Session["NumeroEmpleado"].ToString();
+            }
+            ML.Dependiente dependiente = new ML.Dependiente();
+            ML.Result resultDependientes = BL.Dependiente.GetByNumeroEmpleado(numeroEmpleado);
+            if (resultDependientes.Correct)
+            {
+                Session["NumeroEmpleado"] = numeroEmpleado;
+                dependiente.Dependientes = new List<object>();
+                dependiente.Dependientes = resultDependientes.Objects;
+                return View(dependiente);
+            }
+            else
+            {
+                ViewBag.Mensaje = "Ocurrio un error al cargar los dependientes" + resultDependientes.ErrorMessage;
+                return PartialView("Modal");
+            }
+        }
+        [HttpGet]
+        public ActionResult Form(int? IdDependiente)
+        {
+            ML.Dependiente dependiente = new ML.Dependiente();
+            ML.Result resultDependientesTipo = BL.DependienteTipo.GetAllEF();
+            if (resultDependientesTipo.Correct)
+            {
+                if (IdDependiente == null) //add
+                {
+                    dependiente.Empleado = new ML.Empleado();
+                    dependiente.Empleado.NumeroEmpleado = Session["NumeroEmpleado"].ToString();
+                    dependiente.DependienteTipo = new ML.DependienteTipo();
+                    dependiente.DependienteTipo.DependientesTipo = resultDependientesTipo.Objects;
+                    return View(dependiente);
+                }
+                else  //Update
+                {
+                    ML.Result resultDependiente = BL.Dependiente.GetById(IdDependiente.Value);
+                    if (resultDependiente.Correct)
+                    {
+                        dependiente = ((ML.Dependiente)resultDependiente.Object);
+                        dependiente.DependienteTipo.DependientesTipo = resultDependientesTipo.Objects;
+                        return View(dependiente);
+                    }
+                    else
+                    {
+                        ViewBag.Mensaje = "Ocurrio un error al obtener el dependiente" + resultDependiente.ErrorMessage;
+                        return PartialView("Modal");
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.Mensaje = "Ocurrio un error al obtener algunos datos" + resultDependientesTipo.ErrorMessage;
+                return PartialView("Modal");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Dependiente dependiente)
+        {
+            ML.Result result = new ML.Result();
+            if (dependiente.IdDependiente == 0) //add
+            {
+                result = BL.Dependiente.Add(dependiente);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El Dependiente se agrego Correctamente";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El Dependiente no se agrego Correctamente" + result.ErrorMessage;
+                }
+            }
+            else //update
+            {
+                result = BL.Dependiente.Update(dependiente);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El Dependiente se actualizo Correctamente";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El Dependiente no se actualizo Correctamente" + result.ErrorMessage;
+                }
+            }
+            return PartialView("Modal");
+        }
+        public ActionResult Delete(int IdDependiente)
+        {
+            ML.Result result = BL.Dependiente.Delete(IdDependiente);
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "El dependiente se elimino correctamente";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Ocurrio un error al eliminar" + result.ErrorMessage;
+            }
+            return PartialView("Modal");
+        }
+    }
+}
---
+++ PL/VentaProducto.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Xrm.Sdk.Messages;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace PL.Controllers
+{
+    public class VentaMateriaController : Controller
+    {
+        public ActionResult GetAll()
+        {
+            ML.Materia materia = new ML.Materia();
+            materia.Semestre = new ML.Semestre();
+            materia.Grupo = new ML.Grupo();
+
+            ML.Result result = BL.Materia.GetAll(materia);
+            ML.Result resultSemestre = BL.Semestre.GetAll();
+
+            materia.Semestre.Semestres = resultSemestre.Objects;
+            materia.Materias = result.Objects;
+
+            return View(materia);
+        }
+        [HttpPost]
+        public ActionResult GetAll(ML.Materia materia)
+        {
+            materia.Nombre = (materia.Nombre == null) ? "" : materia.Nombre;
+            //materia.Semestre.IdSemestre = (materia.Semestre.IdSemestre == null) ? "" : empleado.ApellidoPaterno;
+            materia.Grupo.Horario = materia.Grupo.Horario == null ? "" : materia.Grupo.Horario;
+
+            ML.Result result = BL.Materia.GetAll(materia);
+            materia.Materias = result.Objects;
+
+            ML.Result resultSemestre = BL.Semestre.GetAll();
+            materia.Semestre.Semestres = resultSemestre.Objects;
+
+
+            return View(materia);
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CartPost(ML.Materia materia)
+        {
+            ML.VentaMateria ventaMateria = new ML.VentaMateria();
+            ventaMateria.VentaMaterias = new List<object>();
+
+            if (HttpContext.Session.GetString("Producto") == null)
+            {
+                materia.Cantidad = materia.Cantidad = 1;
+                ventaMateria.VentaMaterias.Add(materia);
+                HttpContext.Session.SetString("Producto", Newtonsoft.Json.JsonConvert.SerializeObject(ventaMateria.VentaMaterias));
+                var session = HttpContext.Session.GetString("Producto");
+            }
+            else
+            {
+                var ventaSession = Newtonsoft.Json.JsonConvert.DeserializeObject<List<object>>(HttpContext.Session.GetString("Producto"));
+
+                foreach (var obj in ventaSession)
+                {
+                    ML.Materia objMateria = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Materia>(obj.ToString());
+                    ventaMateria.VentaMaterias.Add(objMateria);
+                }
+                //var indice = ventaMateria.VentaMaterias.IndexOf(materia);
+                foreach (ML.Materia venta in ventaMateria.VentaMaterias.ToList())
+                {
+                    if (materia.IdMateria == venta.IdMateria)
+                    {
+                        venta.Cantidad = venta.Cantidad + 1;   //True                
+                    }
+                    else
+                    {
+                        materia.Cantidad = materia.Cantidad = 1; //False
+                        ventaMateria.VentaMaterias.Add(materia);
+                    }
+                }
+                HttpContext.Session.SetString("Producto", Newtonsoft.Json.JsonConvert.SerializeObject(ventaMateria.VentaMaterias));
+            }
+            if (HttpContext.Session.GetString("Producto") != null)
+            {
+                ViewBag.Message = "Se ha agregado la materia a tu carrito!";
+                return PartialView("Modal");
+            }
+            else
+            {
+                ViewBag.Message = "No se pudo agregar tu producto ):";
+                return PartialView("Modal");
+            }
+
+        }
+        [HttpGet]
+        public ActionResult ResumenCompra(ML.VentaMateria ventaMateria)
+        {
+            if (HttpContext.Session.GetString("Producto") == null)
+            {
+                return View();
+            }
+            else
+            {
+                var ventaSession = Newtonsoft.Json.JsonConvert.DeserializeObject<List<object>>(HttpContext.Session.GetString("Producto"));
+                ventaMateria.VentaMaterias = new List<object>();
+                foreach (var obj in ventaSession)
+                {
+                    ML.Materia objMateria = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Materia>(obj.ToString());
+                    ventaMateria.VentaMaterias.Add(objMateria);
+                }
+
+            }
+
+            return View(ventaMateria);
+        }
+    }
+}
